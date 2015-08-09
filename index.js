c33c75efafb4f4842cf6452247e01017f257e6a6
Joao Paulo Ribeiro <jpnribeiro94@gmail.com>
2015-08-08 23:22:29 -0300

    Demo 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var express = require('express')
 var app = require('./config/express')();
 require('./config/passport')();
-require('./config/database')('mongodb://localhost/voted');
+require('./config/database')('mongodb://localhost/voted'||process.env.MONGOLAB_URI);
 
 http.createServer(app).listen(app.get('port'),function () {
 	console.log('Server on...');
